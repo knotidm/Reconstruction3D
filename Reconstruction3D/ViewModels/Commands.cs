44711fb6d2468251a9818302ebaa3b4b729934b5
wsek <wsek@W-SEK>
2016-06-29 18:19:44 +0200

    viewmodel changes

--- Reconstruction3D/ViewModels/Commands.cs
+++ Reconstruction3D/ViewModels/Commands.cs
@@ -1,13 +1,25 @@
 ﻿using System.Windows.Forms;
 using Commander;
 using PropertyChanged;
+using SharpGL;
+using SharpGL.SceneGraph.Primitives;
+using SharpGL.SceneGraph.Core;
+using System.Collections.ObjectModel;
 
 namespace Reconstruction3D.ViewModels
 {
     [ImplementPropertyChanged]
     public class Commands
     {
+        public ObservableCollection<string> RenderModes { get; set; }
         public string ImagePath { get; set; }
+        public static bool ToonShader { get; set; }
+        public static string SelectedRenderMode { get; set; }
+
+        public Commands()
+        {
+            RenderModes = new ObservableCollection<string> { "Retained Mode", "Immediate Mode" };
+        }
 
         [OnCommand("OpenFile")]
         public void OpenFile()
@@ -21,6 +33,23 @@ namespace Reconstruction3D.ViewModels
                     break;
                 case DialogResult.Cancel:
                     break;
+            }
+        }
+
+        public static void ChangeRenderMode(Scene scene, OpenGL openGL, Axies axies)
+        {
+            switch (SelectedRenderMode)
+            {
+                case "Retained Mode":
+                    {
+                        scene.RenderRetainedMode(openGL, ToonShader); break;
+                    }
+                case "Immediate Mode":
+                    {
+                        axies.Render(openGL, RenderMode.Design);
+                        scene.RenderImmediateMode(openGL);
+                        break;
+                    }
             }
         }
     }
